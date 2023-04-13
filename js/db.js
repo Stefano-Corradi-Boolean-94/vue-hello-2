d56e13b4f11c5e018281bc2b46e0c6ca9a70ccaf
ugodeughi <stefano.corradi@gmail.com>
2023-04-13 13:19:09 +0200

    [add] v-if v-for

---
+++ js/db.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    nome:'Margherita',
+    prezzo: 8,
+    ingredienti:['pomodoro','mozzarella','basilico']
+  },
+  {
+    nome:'Diavola',
+    prezzo: 9,
+    ingredienti:['pomodoro','mozzarella','salame']
+  },
+  {
+    nome:'Marinara',
+    prezzo: 7,
+    ingredienti:['pomodoro','aglio','rosmarino']
+  },
+]
--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const {createApp} = Vue;
+import pizze from './db.js'
 
 
 createApp({
@@ -8,7 +9,16 @@ createApp({
       saluto: 'ciao',
       miaClasse: 'verde',
       altraClasse: 'maiuscolo',
-      counter:0
+      counter:0,
+      image:{
+        src: 'img/bosco.jpg',
+        alt: 'Bosco'
+      },
+      colori:['rosso','verde','blu'],
+      pizze,
+      mostraTesto: true,
+      counterBox:5,
+      clock:'00:00:00'
     }
   },
   
@@ -18,7 +28,43 @@ createApp({
       if(incr) this.counter++;
       else this.counter--;
 
+    },
+
+    mostraColori(){
+      console.log(this.colori)
+      console.log(this.image)
+    },
+
+    getClock(){
+      const d = new Date();
+      this.clock = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+    },
+
+    startClock(){
+
+      // in questo caso devo usare una arrow function per ereditare il this di vue
+      setInterval(() => {
+        this.getClock();
+      },1000)
     }
+
+  },
+
+  mounted(){
+    // questa funzione viene invocata quando l'app è pronta
+    // qui faccio partire delle funzionalità al lanciio dell'app
+    console.log('APP MONTATA');
+    // al mounted attivo le funzioni per la partenza dell'app
+    this.mostraColori();
+    // richiamo starttClock per stampare l'orologio prima della timing funciton
+    this.getClock();
+    // lancio la funzione che attiva la timing function
+    this.startClock();
+  },
+  
+  created(){
+    console.log('APP CREATA');
+    
   }
 
 }).mount('#app')
